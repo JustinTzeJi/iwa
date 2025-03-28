b9f044d7dee9173b7ac65fdc8235cd68951e4418
justintzeji <justintzeji@gmail.com>
2025-03-29 07:10:15 +0800

    fix: resume path

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
         <Resume />
       </article>
       <Button size="sm" asChild className='sticky bottom-5 max-h-screen flex mt-10 md:py-5 rounded-full'>
-        <a href="https://drive.usercontent.google.com/download?id=1m07FQ2oivTbJKHtTf5DWPwLtXXRS8s6x&export=download&authuser=0&confirm=t&uuid=cf63087f-8df9-4b52-b539-e39eebdf4deb&at=AEz70l7uqE3gIH2bZoODXzcI6I1T:1743197644531" target="_blank" rel="noopener noreferrer"> Download Resume
+        <a href="https://1057056f61a8ee08998249b16c1c0f83.r2.cloudflarestorage.com/site/resume_Justin.pdf" target="_blank" rel="noopener noreferrer"> Download Resume
         </a>
       </Button>
     </div>
